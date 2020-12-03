5d3ea7275ac4a3a041150a4d98a9f60b21720082
Nome Sobrenome <joaovfsc231@gmail.com>
2020-12-03 15:48:59 -0300

    react router and blank page Home

--- test_cev/src/App.js
+++ test_cev/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
 
 function App() {
     return (
-        <div>Hello World!</div>
+        <Router>
+            <Switch>
+                <Route path="/" exact component={Home} />
+            </Switch>
+        </Router>
     );
 }
 export default App;
